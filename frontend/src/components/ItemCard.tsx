7de45fd4d9d8f35756cc111b5d647c8eab15103f
duppring327 <senasura03273@gmail.com>
2026-06-08 03:43:32 +0900

    ItemCardのcssを追加。追加のためにvite-envを

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -1,39 +1,42 @@
-import type { item } from "../features/items/itemTypes";
+import type { Item } from "../features/items/itemTypes";
+import "./ItemCard.css";
 
 type ItemCardProps = {
     item: Item;
 };
 
 function ItemCard({ item }: ItemCardProps) {
+    const statusLabel = {
+    available: "募集中",
+    trading: "交渉中",
+    completed: "成立",
+    }[item.status];
+
     return (
         <article className="item-card">
 
-            <div className="item-card__imageArea">
-                <img className="item-card_image" src="{item.imageUrl}" alt="" />
+            <div className="item-card__imag-area">
+                <img className="item-card__image" src="{item.imageUrl}" alt="" />
 
                 <div className="item-card__likes">
-                    <p>♡</p>
-                    <p>item.likes</p>
+                    <span>♡</span>
+                    <span>{item.likes}</span>
                 </div>
             </div>
-            <div className="itemCardBody">
-                <div className="item-card__status">
-                    {item.status === "available" && "募集中"}
-                    {item.status === "trading" && "交渉中"}
-                    {item.status === "completed" && "成立"}
-                </div>
+            <div className="item-card__body">
+                <span className={`item-card__status item-card__status--${item.status}`}>
+                    {statusLabel}
+                </span>
 
                 <h2 className="item-card__title">{item.title}</h2>
 
-                <div className="item-card_bottom">
+                <div className="item-card__bottom">
                     <div className="item-card__price">¥25,000</div>
 
-                    <button className="item-card_favorite" type="button">⭐︎</button>
+                    <button className="item-card__favorite" type="button">⭐︎</button>
                 </div>
-
-                
             </div>
-
         </article>
-    )
+    );
 }
+export default ItemCard;
---
+++ frontend/src/screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import ItemCard from "../components/ItemCard";
+import { demoItems } from "../features/items/itemData";
+
+function HomeScreen() {
+    return (
+        <section>
+        {demoItems.map((item) => (
+            <ItemCard key={item.id} item={item} />
+        ))}
+        </section>
+    );
+}
+
+export default HomeScreen;
